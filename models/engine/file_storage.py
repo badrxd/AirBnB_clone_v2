a0fbf529068d676dddabab5afe3bdfe1f82226bc
badrxd <badrxdxd@gmail.com>
2023-09-17 23:56:18 +0100

    ok

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,7 +52,10 @@ class FileStorage:
     def delete(self, obj=None):
         """ delete obj from __objects if it’s inside """
         if obj is not None:
-            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-            if key in FileStorage.__objects:
-                FileStorage.__objects.pop(key)
-            self.save()
+            try:
+                key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+                if key in FileStorage.__objects:
+                    FileStorage.__objects.pop(key)
+                    self.save()
+            except AttributeError:
+                pass
