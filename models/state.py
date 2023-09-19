80d15fd0a2dfe3c93cbe4cdebb26d9514b19f091
badrxd <badrxdxd@gmail.com>
2023-09-19 21:49:27 +0100

    ok

--- models/state.py
+++ models/state.py
@@ -1,28 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan')
-
-    #def __init__(self, name):
-        #self.name = name
-
-    @property
-    def cities(self):
-        """Returns the cities"""
-        from models import storage
-        city_list = []
-        for key, value in storage.all(City).items():
-            if self.id == value.state_id:
-                city_list.append(value)
-        return city_list
+    name = ""
