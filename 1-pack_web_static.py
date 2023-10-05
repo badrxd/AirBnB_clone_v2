6cf4c52bfc1d2285f94adc40bf8681c0696fcce5
badrxd <badrxdxd@gmail.com>
2023-10-05 16:57:03 +0100

    task 1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,20 +3,13 @@
 from the contents of the web_static folder of your AirBnB
 Clone repo, using the function do_pack.
 """
-import time
-from os import path
+
+from fabric.decorators import task
 from fabric.api import local
 
 
+@task
 def do_pack():
-    """create tgz"""
-
-    try:
-        if path.isdir("versions") is False:
-            local("mkdir-p versions")
-        date = time.strftime('%Y%m%d%H%M%S')
-        fileName = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(fileName))
-        return fileName
-    except Exception:
-        return None
+    """ create .tgz archive """
+    local("mkdir -p versions ; tar -cvzf \
+versions/web_static_$(date +%Y%m%d%H%M%S).tgz web_static/")
