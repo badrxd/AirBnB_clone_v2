3dc780cf6c81c019d8563ee89a2889532321c1a8
badrxd <badrxdxd@gmail.com>
2023-10-20 01:45:39 +0100

    task3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -19,5 +19,11 @@ def hello_world_3(text):
     return "C {}".format(text.replace("_", " "))
 
 
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hello_world_4(text='is cool'):
+    return "C {}".format(text.replace("_", " "))
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
