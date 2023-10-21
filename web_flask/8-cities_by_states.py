b52bab230ec060694a6d722853e7eaf1f2f4b494
badrxd <badrxdxd@gmail.com>
2023-10-21 19:37:58 +0100

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""cript that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def hello_world():
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_request(error):
+    print(error)
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
