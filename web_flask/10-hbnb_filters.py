cf19cd527129921a8efe01813b5a5e3607c259e3
root <root@DESKTOP-J73E3I4>
2023-10-22 00:58:53 +0100

    add 11. HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""cript that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hello_world():
+    states = storage.all(State)
+    amenity = storage.all(Amenity)
+    print(amenity)
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenity=amenity)
+
+
+@app.teardown_appcontext
+def teardown_request(error):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
